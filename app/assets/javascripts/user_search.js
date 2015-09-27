17feba79ddfa23ecde1dbd5a1ee5c63bbfbbcb0d
jon-elofson <jonathan.elofson@gmail.com>
2015-09-26 20:57:35 -0400

    finish autocomplete search bar

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -2,7 +2,37 @@ $.UserSearch = function (el) {
   this.$el = $(el);
   this.$input = this.$el.find('input');
   this.$ul = this.$el.find('ul');
+  this.users = [];
+  this.$input.on('input',this.handleInput.bind(this));
+};
 
+$.UserSearch.prototype.render = function () {
+  this.$ul.empty();
+  var that = this;
+  this.users.forEach(function (user) {
+    var $li = $('<li>');
+    $li.html(user.username);
+    that.$ul.append($li);
+  });
+};
+
+$.UserSearch.prototype.handleInput = function (event) {
+  event.preventDefault();
+  $form = event.currentTarget;
+  var that = this;
+  var input_str = $form.value;
+  $.ajax({
+    url: "search",
+    type: "GET",
+    dataType: "json",
+    data: {
+      query: input_str
+    },
+    success: function (data) {
+      that.users = data;
+      that.render();
+    }
+  });
 };
 
 
